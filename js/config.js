5ceaa8a1c7cf495877e438635be85dfa9b96242f
Khushi <158452708+Khushi8929@users.noreply.github.com>
2024-12-11 23:15:57 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,9 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: '2nr8j77er3nd2qtcamhr3t1lph', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'eu-north-1_eKEL8Jl2E
+', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
